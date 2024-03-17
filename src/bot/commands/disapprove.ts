f2c5a053357b26569f8ae693425e118a45a2f59b
tuhana <git@tuhana.me>
2024-03-17 17:15:24 +0300

    `deno lint`

--- src/bot/commands/disapprove.ts
+++ src/bot/commands/disapprove.ts
@@ -2,7 +2,7 @@ import { CommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from '@n
 
 import { BouncerSlashCommandBuilder, Command } from './@index.ts';
 import { checkInteractionMember, endInterview } from './@helpers.ts';
-import { InterviewStatus, InterviewType } from '../../database.ts';
+import { InterviewStatus } from '../../database.ts';
 import { BouncerBot } from '../bouncer.ts';
 
 export default class Disapprove implements Command {
