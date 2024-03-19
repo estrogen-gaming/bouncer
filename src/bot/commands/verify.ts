46d414ac1df31e53e6be9c3caf413020c9b0542a
tuhana <git@tuhana.me>
2024-03-20 02:38:36 +0300

    `deno lint`

--- src/bot/commands/verify.ts
+++ src/bot/commands/verify.ts
@@ -1,9 +1,7 @@
 import { CommandInteraction, SlashCommandBuilder } from '@npm/discord.js';
 
 import { BouncerSlashCommandBuilder, Command } from './@index.ts';
-import { checkInteractionMember, startInterview, startSelfIDInterview } from './@helpers.ts';
-import { InterviewStatus, InterviewType, UserData } from '../../database.ts';
-import { BouncerBot } from '../bouncer.ts';
+import { startSelfIDInterview } from './@helpers.ts';
 
 export default class Verify implements Command {
   public command(): BouncerSlashCommandBuilder {
