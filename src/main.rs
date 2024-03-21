6f54ba5b08357f7b8c29104f4f4cd2a43c77377e
tuhana <git@tuhana.me>
2024-03-21 23:01:31 +0300

    Use `eyre` and `color-eyre` for printing errors

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,25 @@ use figment::{
 mod cli;
 mod config;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+
     match cli::Cli::parse().subcommand {
         cli::SubCommands::Start { config } => {
             if !config.try_exists()? {
-                return Err("Config file does not exist".into());
+                eprintln!("Config file does not exist");
+                std::process::exit(1);
             }
 
-            let _config = Figment::new()
+            let config = Figment::new()
                 .merge(Yaml::file(config))
                 .merge(Env::raw().split("__"))
-                .extract::<config::Config>()?;
+                .extract::<config::Config>();
+
+            if let Err(err) = config {
+                eprintln!("Error parsing config: {err}");
+                std::process::exit(1);
+            }
 
             println!("Hello, world!");
         }
