cc4993e27c8fabf0a5d8ef4fa573784269f5def6
tuhana <git@tuhana.me>
2024-03-21 22:56:22 +0300

    Create config schemas

---
+++ src/config.rs
@@ -0,0 +1,68 @@
+use std::path::PathBuf;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    /// The path of the SQLite database file.
+    #[serde(default = "default_database_path")]
+    pub database: PathBuf,
+    #[serde(default = "default_logs_folder_path")]
+    /// The path of the log folder.
+    pub logs_folder: PathBuf,
+
+    /// Discord specific configurations.
+    pub discord: Discord,
+}
+
+/// Default path for the SQLite database.
+fn default_database_path() -> PathBuf {
+    PathBuf::from("data/db.sqlite")
+}
+
+/// Default path for the logs folder.
+fn default_logs_folder_path() -> PathBuf {
+    PathBuf::from("logs/")
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Discord {
+    // TODO: Maybe a custom type for Discord bot token
+    // that will have a `parse` function to validate
+    // the token.
+    /// Token for Discord bot.
+    pub token: String,
+    /// Server ID where bot operates.
+    pub server_id: i64,
+
+    /// IDs for Discord channels.
+    pub channels: DiscordChannels,
+    /// IDs for Discord roles.
+    pub roles: DiscordRoles,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DiscordChannels {
+    //* Categories are technically channels in Discord.
+    /// Category ID for interview channels.
+    pub interviews_category_id: i64,
+
+    /// Channel ID for user mark messages.
+    pub interview_marks_id: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DiscordRoles {
+    /// Role IDs for interviewers.
+    pub interviewers_id: Vec<i64>,
+
+    /// Role ID for users awaiting interview.
+    pub pending_interview_id: i64,
+    /// Role ID for users currently being interviewed.
+    pub ongoing_interview_id: i64,
+
+    /// Role ID for text-verified users.
+    pub text_verified_id: i64,
+    /// Role ID for ID-verified users.
+    pub id_verified_id: i64,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use clap::Parser;
-
+use figment::{
+    providers::{Env, Format, Yaml},
+    Figment,
+};
 mod cli;
+mod config;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     match cli::Cli::parse().subcommand {
         cli::SubCommands::Start { config } => {
-            println!("Hello, world! Specified config path: {}", config.display());
+            if !config.try_exists()? {
+                return Err("Config file does not exist".into());
+            }
+
+            let _config = Figment::new()
+                .merge(Yaml::file(config))
+                .merge(Env::raw().split("__"))
+                .extract::<config::Config>()?;
+
+            println!("Hello, world!");
         }
     }
+
+    Ok(())
 }
