2a56adf654601d45abf5ab993c7d9f54f7ceeb0c
tuhana <git@tuhana.me>
2024-03-25 05:44:09 +0300

    Write a custom tracing `Writer` for logging WARN and ERROR level logs to `stderr` instead of `stdout`

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -1,14 +1,72 @@
-use std::path::PathBuf;
+use std::{io, path::PathBuf};
 
-use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter};
+use tracing::{Level, Metadata};
+use tracing_subscriber::{
+    fmt::{self, MakeWriter},
+    layer::SubscriberExt,
+    EnvFilter,
+};
+
+struct BouncerStdWriter {
+    stdout: io::Stdout,
+    stderr: io::Stderr,
+}
+
+enum StdIOLock<'a> {
+    Stdout(io::StdoutLock<'a>),
+    Stderr(io::StderrLock<'a>),
+}
+
+impl BouncerStdWriter {
+    fn new() -> Self {
+        Self {
+            stdout: io::stdout(),
+            stderr: io::stderr(),
+        }
+    }
+}
+
+impl<'a> io::Write for StdIOLock<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self {
+            Self::Stdout(stdout) => stdout.write(buf),
+            Self::Stderr(stderr) => stderr.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        match self {
+            Self::Stdout(stdout) => stdout.flush(),
+            Self::Stderr(stderr) => stderr.flush(),
+        }
+    }
+}
+
+impl<'a> MakeWriter<'a> for BouncerStdWriter {
+    type Writer = StdIOLock<'a>;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        StdIOLock::Stdout(self.stdout.lock())
+    }
+
+    fn make_writer_for(&'a self, meta: &Metadata<'_>) -> Self::Writer {
+        if meta.level() <= &Level::WARN {
+            return StdIOLock::Stderr(self.stderr.lock());
+        }
+
+        StdIOLock::Stdout(self.stdout.lock())
+    }
+}
 
 /// Sets-up [`tracing`](https://docs.rs/tracing) for logging to both
 /// [`std::io::stdout()`](https://doc.rust-lang.org/stable/std/io/fn.stdout.html)
 /// and [`tracing_appender::rolling::hourly()`](https://docs.rs/tracing-appender/latest/tracing_appender/rolling/fn.hourly.html).
 pub fn set_up(logs_folder: Option<PathBuf>) -> eyre::Result<()> {
+    let std_writer = BouncerStdWriter::new();
+
     let collector = tracing_subscriber::registry()
         .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("bouncer=info")))
-        .with(fmt::Layer::new().with_writer(std::io::stdout))
+        .with(fmt::Layer::new().with_writer(std_writer))
         .with(
             fmt::Layer::new()
                 .with_ansi(false)
